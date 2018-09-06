6bba76e5280c6dd6740392a748a6e1523de8cbbc
Eloy <eramirezhernanz@hawk.iit.edu>
2018-09-06 23:41:03 +0200

    Read Changes

--- assign1/storage_mgr.h
+++ assign1/storage_mgr.h
@@ -49,13 +49,46 @@ extern RC destroyPageFile (char *fileName){
 }
 
 /* reading blocks from disc */
-extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern int getBlockPos (SM_FileHandle *fHandle);
-extern RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
+    if(fHandle->totalNumPages < pageNum){
+        return RC_READ_NON_EXISTING_PAGE;
+    } else {
+        fread(memPage, PAGE_SIZE, fHandle->totalNumPages, fhandle->*fileName);
+    }
+}
+extern int getBlockPos (SM_FileHandle *fHandle){
+    return (fhandle->curPagePos);
+}
+extern RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    readBlock(0, fHandle, memPage);
+}
+extern RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    if(getBlockPos(fHandle) == 0){
+        return RC_READ_NON_EXISTING_PAGE;
+    } else {
+        readBlock(getBlockPos(fHandle)-1, fHandle, memPage);
+    }
+}
+extern RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    
+    readBlock(fHandle->curPagePos, fHandle, memPage);
+
+}
+extern RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    if(getBlockPos(fHandle)==(PAGE_SIZE-1){
+        return RC_READ_NON_EXISTING_PAGE;
+    } else {
+        readBlock(getBlockPos(fHandle) + 1, fHandle, memPage);
+        
+    }
+    
+}
+extern RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    readBlock((PAGE_SIZE-1), fHandle, memPage);
+    
+}
+    
+    
 
 /* writing blocks to a page file */
 extern RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
